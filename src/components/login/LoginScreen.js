e6a5c704711f72003fdd1d321181ce3995762a4d
jaravad <jaravad@uninorte.edu.co>
2021-03-10 17:53:55 -0500

    login button, history.push and history.replace

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,7 +1,28 @@
-export const LoginScreen = () => {
+import React, { useEffect } from 'react';
+
+/* React Router passes some props to the components being routed,
+  that's why 'history' prop is available here */
+export const LoginScreen = ({ history }) => {
+  useEffect(() => {
+    document.title = 'Heroes App - Login';
+  }, []);
+
   return (
-    <>
-      <h1>LoginScreen</h1>
-    </>
+    <div className="container py-4">
+      <h1 className="pb-2 border-bottom mb-3">LoginScreen</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          // * Add a history entry
+          // history.push('/');
+          // Replace current history entry
+          history.replace('/');
+          /* When replace is used browser's back button won't redirect 
+          to the previous history entry as this one was replaced*/
+        }}
+      >
+        Login
+      </button>
+    </div>
   );
 };
