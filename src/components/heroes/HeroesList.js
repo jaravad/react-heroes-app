e4f4a5b44b323bd24279fa2ba2d1e86da2e6db43
jaravad <jaravad@uninorte.edu.co>
2021-03-11 12:01:56 -0500

    use masonry from package manager instead of cdn, improve masonry initialization

--- src/components/heroes/HeroesList.js
+++ src/components/heroes/HeroesList.js
@@ -1,14 +1,23 @@
+import { useRef, useLayoutEffect } from 'react';
+
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 import { HeroCard } from './HeroCard';
+import Masonry from 'masonry-layout';
 
 export const HeroesList = ({ publisher }) => {
+  const ref = useRef(null);
   const heroes = getHeroesByPublisher(publisher);
 
+  // initialize masonry layout
+  useLayoutEffect(() => {
+    new Masonry(ref.current, {
+      // options
+      percentPosition: true,
+    });
+  }, []);
+
   return (
-    <div
-      className="row"
-      data-masonry={JSON.stringify({ percentPosition: true })}
-    >
+    <div id="masonryGrid" className="row" ref={ref}>
       {heroes.map((hero) => {
         return <HeroCard key={hero.id} {...hero} />;
       })}
