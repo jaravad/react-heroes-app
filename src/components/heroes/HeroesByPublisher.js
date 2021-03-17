62bb1cb179aec15e07dd500be7947e9a38098cf2
jaravad <jaravad@uninorte.edu.co>
2021-03-16 21:52:51 -0500

    refactor: reuse logic to display heroes by publisher

--- src/components/heroes/HeroesByPublisher.js
+++ src/components/heroes/HeroesByPublisher.js
@@ -2,12 +2,12 @@ import { useMemo } from 'react';
 import { HeroesList } from '../heroes/HeroesList';
 import { getHeroesByPublisher } from '../../selectors/getHeroesByPublisher';
 
-export const MarvelScreen = () => {
-  const heroes = useMemo(() => getHeroesByPublisher('Marvel Comics'), []);
+export const HeroesByPublisher = ({ publisher }) => {
+  const heroes = useMemo(() => getHeroesByPublisher(publisher), [publisher]);
 
   return (
     <div className="fade-anim">
-      <h1>MarvelScreen</h1>
+      <h1 className="pb-2 border-bottom">{publisher}</h1>
       <HeroesList heroes={heroes} />
     </div>
   );
--- src/routers/AuthenticatedRoutes.js
+++ src/routers/AuthenticatedRoutes.js
@@ -1,10 +1,9 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import { NavBar } from '../components/ui/NavBar';
-import { MarvelScreen } from '../components/marvel/MarvelScreen';
-import { DCScreen } from '../components/dc/DCScreen';
 import { HeroScreen } from '../components/heroes/HeroScreen';
 import { SearchScreen } from '../components/search/SearchScreen';
+import { HeroesByPublisher } from '../components/heroes/HeroesByPublisher';
 
 export const AuthenticatedRoutes = () => {
   return (
@@ -13,10 +12,18 @@ export const AuthenticatedRoutes = () => {
 
       <div className="container py-4">
         <Switch>
-          <Route exact path="/marvel" component={MarvelScreen} />
-          <Route exact path="/dc" component={DCScreen} />
+          <Route
+            exact
+            path="/marvel"
+            component={() => <HeroesByPublisher publisher="Marvel Comics" />}
+          />
+          <Route
+            exact
+            path="/dc"
+            component={() => <HeroesByPublisher publisher="DC Comics" />}
+          />
           <Route exact path="/hero/:heroId" component={HeroScreen} />
-          <Route exact path="/search" component={SearchScreen}/>
+          <Route exact path="/search" component={SearchScreen} />
 
           <Redirect to="/marvel" />
         </Switch>
