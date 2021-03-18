4bba6c0906ce1ac32744fcd35417a2827da27b42
jaravad <jaravad@uninorte.edu.co>
2021-03-18 17:04:35 -0500

    feat(auth): setup auth reducer and auth context

--- src/HeroesApp.js
+++ src/HeroesApp.js
@@ -1,11 +1,19 @@
+import { useReducer } from 'react';
 import { AppRouter } from './routers/AppRouter';
+import { AuthContext } from './auth/AuthContext';
+import { authReducer } from './auth/authReducer';
 
 import './styles.css';
 
+const init = () => {
+  return JSON.parse(localStorage.getItem('user')) || { logged: false };
+};
+
 export const HeroesApp = () => {
+  const [user, dispatch] = useReducer(authReducer, {}, init);
   return (
-    <>
+    <AuthContext.Provider value={{ user, dispatch }}>
       <AppRouter />
-    </>
+    </AuthContext.Provider>
   );
 };
