a662db44a37a8d37c98d87a65a5e753494a2c431
jaravad <jaravad@uninorte.edu.co>
2021-03-18 15:54:59 -0500

    refactor: remove framer motion

--- src/components/heroes/HeroCard.js
+++ src/components/heroes/HeroCard.js
@@ -1,32 +1,24 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
 import { HeroInfo } from './HeroInfo';
-import { motion, AnimatePresence } from 'framer-motion';
 
 export const HeroCard = memo(function HeroCard({ hero }) {
   return (
-    <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        className="col-md-6 col-xl-4 p-3"
+    <div className="col-md-6 col-xl-4 p-3 fade-anim">
+      <Link
+        to={`/hero/${hero.id}`}
+        className="row g-0 border rounded overflow-hidden shadow-sm hero-card"
       >
-        <Link
-          to={`/hero/${hero.id}`}
-          className="row g-0 border rounded overflow-hidden shadow-sm hero-card"
-        >
-          <div className="col-5 position-relative">
-            <div className="img-overlay w-100 h-100 position-absolute"></div>
-            <img
-              src={`./assets/${hero.id}.jpg`}
-              className="object-fit-cover w-100 h-100 position-absolute"
-              alt={hero.superhero}
-            />
-          </div>
-          <HeroInfo {...hero} className="px-3 py-4 col-7" showMore />
-        </Link>
-      </motion.div>
-    </AnimatePresence>
+        <div className="col-5 position-relative">
+          <div className="img-overlay w-100 h-100 position-absolute"></div>
+          <img
+            src={`./assets/${hero.id}.jpg`}
+            className="object-fit-cover w-100 h-100 position-absolute"
+            alt={hero.superhero}
+          />
+        </div>
+        <HeroInfo {...hero} className="px-3 py-4 col-7" showMore />
+      </Link>
+    </div>
   );
 });
