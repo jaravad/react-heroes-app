37beb8c5fe2fed9f4937dd6452ee3b1d81962136
jaravad <jaravad@uninorte.edu.co>
2021-03-19 01:43:44 -0500

    feat(auth): create PrivateRoute component to protect private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,24 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../auth/AuthContext';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { AuthenticatedRoutes } from './AuthenticatedRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
+  const {
+    user: { logged },
+  } = useContext(AuthContext);
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={LoginScreen} />
-        <Route path="/" component={AuthenticatedRoutes} />
+        <PrivateRoute
+          path="/"
+          component={AuthenticatedRoutes}
+          isAuthenticated={logged}
+        />
       </Switch>
     </Router>
   );
